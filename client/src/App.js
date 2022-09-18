48d49aaf037129909939113b3e127c94f3c51221
SD <sourish.d@gmail.com>
2022-09-18 18:37:06 -0500

    h1 moved into formelement

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import styles from './App.module.css'
 import DataTable from './DataTable/DataTable';
 import FormElement from './FormElement/FormElement';
+
+
 function App() {
   return (
     <div className={styles.container}>
-      <h1>Employee Data Entry</h1>
       <FormElement/>
       <DataTable/>
     </div>
