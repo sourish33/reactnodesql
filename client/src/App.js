67ced5a830dbf96fd8ad8dcde8a959b50b48832a
SD <sourish.d@gmail.com>
2022-09-18 19:05:20 -0500

    imported browswer router, routes and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import styles from './App.module.css'
 import DataTable from './DataTable/DataTable';
 import FormElement from './FormElement/FormElement';
+import {Route, Routes} from 'react-router-dom'
 
 
 function App() {
