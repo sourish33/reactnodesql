e7eb1dfa0aedcdd4049fd88818b399b460bd5e4b
SD <sourish.d@gmail.com>
2022-09-19 00:21:05 -0500

    get request on server works

--- server/app.js
+++ server/app.js
@@ -1,28 +1,33 @@
-const express = require('express')
+const express = require("express")
 const PORT = 3001
-
 
 const app = express()
 
 // get the client
-const mysql = require('mysql2');
+const mysql = require("mysql2/promise")
 
-// create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'employees',
-  password:'jcU96mo'
-});
+// get the promise implementation, we will use bluebird
+// const bluebird = require("bluebird")
 
-// execute will internally call prepare and query
-connection.execute(
-    'SELECT * FROM employee_table', 
-    function(err, results, fields) {
-      console.log(results); // results contains rows returned by server
-      //console.log(fields); // fields contains extra meta data about results, if available
-    })
+app.get("/users", async (req, res) => {
+    try {
+        // create the connection to database
+        const connection = await mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            database: "employees",
+            password: "jcU96mo",
+        })
 
-app.listen(PORT, ()=>{
+        const [rows, fields] = await connection.execute(
+            "SELECT * FROM employee_table"
+        )
+        return res.status(200).json(rows)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`)
 })
