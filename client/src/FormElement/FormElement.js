3664ee3aceb77d6d1c6e10685ed00748d4663d9d
SD <sourish.d@gmail.com>
2022-09-20 10:32:11 -0500

    form width controlled with row and col

--- client/src/FormElement/FormElement.js
+++ client/src/FormElement/FormElement.js
@@ -28,63 +28,66 @@ function FormElement() {
 
     return (
 
-        <form className="mt-4 ml-0">
-                <h2>Enter Employee Data</h2>
-                    <label for='fname' className="form-label">First Name:</label>
-                    <input
-                        type="text"
-                        name="fname"
-                        className="form-control"
-                        value={formData.fname}
-                        onChange={(e) =>
-                            handleChange(e)
-                        }>
-                    </input>
-    
-                    
-                    <label for='lname' className="form-label">Last Name:</label>
-                    <input
-                        type="text"
-                        name="lname"
-                        className="form-control"
-                        value={formData.lname}
-                        onChange={(e) =>
-                            handleChange(e)
-                        }
-                    ></input>
-                    <label for='age' className="form-label">Age:</label>
-                    <input
-                        type="number"
-                        name="age"
-                        className="form-control"
-                        value={formData.age}
-                        onChange={(e) =>
-                            handleChange(e)
-                        }
-                    ></input>
-                    <label for='job_title' className="form-label">Job Title:</label>
-                    <input
-                        type="text"
-                        name="job_title"
-                        className="form-control"
-                        value={formData.job_title}
-                        onChange={(e) =>
-                            handleChange(e)
-                        }
-                    ></input>
-                    <label for='salary' className="form-label">Salary:</label>
-                    <input
-                        type="number"
-                        name="salary"
-                        className="form-control"
-                        value={formData.salary}
-                        onChange={(e) =>
-                            handleChange(e)
-                        }
-                    ></input>
-                
-                <div><button type="button" className="btn btn-primary mt-2" onClick={handleClick}>Submit</button></div>
-            </form>
+        <div className="row">
+            <form className="mt-4 ml-0">
+                    <h2>Enter Employee Data</h2>
+                        <div className="col-lg-6">
+                            <label for='fname' className="form-label">First Name:</label>
+                            <input
+                                type="text"
+                                name="fname"
+                                className="form-control"
+                                value={formData.fname}
+                                onChange={(e) =>
+                                    handleChange(e)
+                                }>
+                            </input>
+            
+                            <label for='lname' className="form-label">Last Name:</label>
+                            <input
+                                type="text"
+                                name="lname"
+                                className="form-control"
+                                value={formData.lname}
+                                onChange={(e) =>
+                                    handleChange(e)
+                                }
+                            ></input>
+                            <label for='age' className="form-label">Age:</label>
+                            <input
+                                type="number"
+                                name="age"
+                                className="form-control"
+                                value={formData.age}
+                                onChange={(e) =>
+                                    handleChange(e)
+                                }
+                            ></input>
+                            <label for='job_title' className="form-label">Job Title:</label>
+                            <input
+                                type="text"
+                                name="job_title"
+                                className="form-control"
+                                value={formData.job_title}
+                                onChange={(e) =>
+                                    handleChange(e)
+                                }
+                            ></input>
+                            <label for='salary' className="form-label">Salary:</label>
+                            <input
+                                type="number"
+                                name="salary"
+                                className="form-control"
+                                value={formData.salary}
+                                onChange={(e) =>
+                                    handleChange(e)
+                                }
+                            ></input>
+                        </div>
+            
+                    <div><button type="button" className="btn btn-primary mt-2" onClick={handleClick}>Submit</button></div>
+                </form>
+        </div>
     )
 }
 
